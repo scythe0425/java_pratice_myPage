92c895acf021f9bfcb12632c01f26299299c7086
scyth0425 <scythe0425@naver.com>
2025-07-11 16:50:24 +0900

    update

---
+++ src/com/example/HelloServlet.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h2>Hello, Servlet! (web.xml 매핑)</h2>");
+        out.println("</body></html>");
+    }
+}
